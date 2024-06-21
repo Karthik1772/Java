6eaa3ae1bb0c53bfde07ddadeba6c69c71116bec
Karthik1772 <karthikamma2004gmail.com>
2024-06-21 11:37:55 +0530

    completed

--- basic.java
+++ basic.java
@@ -1,5 +1,13 @@
+// public class basic {
+//     public static void main(String[] args){
+//         System.out.print("Hello World !!! 1");
+//         System.out.print(" Hello World !!! 2");
+//     }
+// }//for printing single line
+
 public class basic {
     public static void main(String[] args){
-        System.out.println("Hello World !!!");
+        System.out.println("Hello World !!! 1");
+        System.out.println("Hello World !!! 2");
     }
-}
+}//for printing in seperate lines
