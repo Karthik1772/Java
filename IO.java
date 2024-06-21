0959b982f4c6c202bec361709cd1d434e599339d
Karthik1772 <karthikamma2004gmail.com>
2024-06-21 12:23:25 +0530

    completed

---
+++ IO.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class IO{
+    public static void main(String[] args) {
+        System.out.println("what is your age");
+        Scanner input =new Scanner(System.in);
+        System.out.println("your age is "+input.nextInt());
+    }
+}
