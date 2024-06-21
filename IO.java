61aa54626bcf1d30a64e90b13202f10af9e8eda6
Karthik1772 <karthikamma2004gmail.com>
2024-06-21 18:44:31 +0530

    completed

--- IO.java
+++ IO.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class IO{
     public static void main(String[] args) {
         System.out.println("what is your age");
-        Scanner input =new Scanner(System.in);
-        System.out.println("your age is "+input.nextInt());
+        Scanner input = new Scanner(System.in);
+        System.out.println("your age is "+ input.nextInt());
     }
 }
