57cae4927065b6aa1aadf8a209efa1e7f56f94f7
karthik1772 <karthikamma2004@gmail.com>
2025-02-18 22:07:50 +0530

    completed

--- training/day4/num_log18.java
+++ training/day4/num_log18.java
@@ -1,15 +1,15 @@
-//encode set of all odd digit and even digit
+//encode set of all even digit
 
 import java.util.Scanner;
 
-public class num_log17 {
+public class num_log18 {
     public static void main(String[] args) {
         Scanner scan =new Scanner(System.in);
         System.out.println("enter the number");
         int a = scan.nextInt();
         while (a != 0) {
             int b = a % 10;
-            if (b % 2 != 0) {
+            if (b % 2 == 0) {
                 System.out.print(b);
             }
             a = a / 10;
