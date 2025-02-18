f39b1d891c05d2a8a5a0d921429dba7b4bace5e6
karthik1772 <karthikamma2004@gmail.com>
2025-02-18 16:19:07 +0530

    completed

--- training/day4/num_log11.java
+++ training/day4/num_log11.java
@@ -1,14 +1,18 @@
 import java.util.Scanner;
 //find the max no in a given no
-//find the min no in a given no
-//print even no
-//print odd no
-//find the sum of a given no 395683
-//find the product of a given no 395683
-//encode set of all odd digit and even digit
 public class num_log11 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("enter the number");
+        int num = scan.nextInt();
+        int max=0,rem=0;
+        while(num!=0){
+            rem=num%10;
+            if(rem>max){
+                max=rem;
+            }
+            num=num/10;
+        }
+        System.out.println(max);
     }
 }
---
+++ training/day4/num_log12.java
@@ -0,0 +1,11 @@
+package training.day4;
+//find the min no in a given no
+//print even no
+//print odd no
+//find the sum of a given no 395683
+//find the product of a given no 395683
+//encode set of all odd digit and even digit
+
+public class num_log12 {
+    
+}
