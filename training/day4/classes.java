a9eba3b0a0e674dbb35ef0bb6230c7c3e04ad181
karthik1772 <karthikamma2004@gmail.com>
2025-02-18 14:53:08 +0530

    completed

---
+++ training/day4/classes.java
@@ -0,0 +1,21 @@
+public class classes{
+
+    public static void main(String[]b ){
+    System.out.println(Xyz.a);
+    Xyz x=new Xyz();
+    Xyz y=new Xyz();
+    Xyz z=new Xyz();
+    x.b=90;
+    System.out.println(x.a+" "+x.b);
+    System.out.println(y.a+" "+y.b);
+    System.out.println(z.a+" "+z.b);
+    }
+    
+    }
+    class Xyz{
+    static int a=10;
+    int b=20;
+    }
+    class Mno{
+    
+    }
