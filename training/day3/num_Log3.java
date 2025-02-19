cc6a8adc54b14a8cf736f7bf9a2088542251eea2
karthik1772 <karthikamma2004@gmail.com>
2025-02-19 14:25:46 +0530

    completed

--- training/day3/num_Log3.java
+++ training/day3/num_Log3.java
@@ -1,16 +1,20 @@
-//ip=1234 & 77 op=123477
+
 import java.util.Scanner;
 
-public class num_Log3 {
+public class num_log3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number n");
         int n = scan.nextInt();
         System.out.println("Enter the number m");
         int m = scan.nextInt();
-        int res,ans;
-        res=n*100;
-        ans=res+m;
-        System.out.println(ans);
+        int rem = 1, op, count = m;
+        while (m >0) {//81174//8117//811/81/8
+            m = m / 10;//8117//811//81//8/0
+            rem = rem * 10;//10//100//1000/10000/100000
+            
+        }
+        op = (n * rem) + count;//99456*100000+81174
+        System.out.println(op);
     }
 }
--- training/day3/num_log4.java
+++ training/day3/num_log4.java
@@ -1,15 +1,35 @@
+
 //ip=1234 & 77 op=1277
 import java.util.Scanner;
 
 public class num_log4 {
-     public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number n");
         int n = scan.nextInt();
         System.out.println("Enter the number m");
         int m = scan.nextInt();
-        int res = (n/100)*100;
-        int ans = res + m;
+        int res, ans = 0;
+        if (n > 0 && m > 0) {
+            res = (n / 100) * 100;
+            ans = res + m;
+        } else if (n < 0 && m > 0) {
+            n = -n;
+            res = (n / 100) * 100;
+            ans = res + m;
+            ans = -ans;
+        } else if (n > 0 && m < 0) {
+            res = (n / 100) * 100;
+            m = -m;
+            ans = res + m;
+            ans = -ans;
+        } else if (n < 0 && m < 0) {
+            m = -m;
+            n = -n;
+            res = (n / 100) * 100;
+            ans = res + m;
+            ans = -ans;
+        }
         System.out.println(ans);
     }
 }
