11eb8d1f5968525e5b99d7557ece8da9a85a8697
karthik1772 <karthikamma2004@gmail.com>
2025-02-20 10:31:13 +0530

    completed

--- training/day4/num_log18.java
+++ training/day4/num_log18.java
@@ -3,14 +3,30 @@
 import java.util.Scanner;
 
 public class num_log18 {
+    public static int reverse(int a) {
+        int rev = 0, rem = 0;
+        while (a != 0) {
+            rem = a % 10;
+            a = a / 10;
+            rev = rev * 10 + rem;
+        }
+        return rev;
+    }
+
     public static void main(String[] args) {
-        Scanner scan =new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("enter the number");
         int a = scan.nextInt();
+        a = reverse(a);
+        boolean first = true;
         while (a != 0) {
             int b = a % 10;
             if (b % 2 == 0) {
+                if (!first) {
+                    System.out.print(",");
+                }
                 System.out.print(b);
+                first = false;
             }
             a = a / 10;
         }
