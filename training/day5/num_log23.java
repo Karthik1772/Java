4fbfd05aec2e596d7c6c1aa135c0a59a80b56fe0
karthik1772 <karthikamma2004@gmail.com>
2025-02-23 12:04:41 +0530

    completed

--- training/day5/num_log23.java
+++ training/day5/num_log23.java
@@ -7,12 +7,12 @@ public class num_log23 {
     static void ramanujan(int range) {
         for (int i = 1; i * i * i < range; i++) {
             for (int j = i + 1; j * j * j < range; j++) {
-                int s1=i*i*i+j*j*j;
-                for(int k=i+1;k*k*k<range;k++){
-                    for(int l=k+1;l*l*l<range;l++){
-                        int s2=k*k*k+l*l*l;
-                        if(s1==s2){
-                            System.out.println(i+"^3 + " + j+"^3 == "+k+"^3 + "+l+"^3 = "+s1 );
+                int s1 = i * i * i + j * j * j;
+                for (int k = i + 1; k * k * k < range; k++) {
+                    for (int l = k + 1; l * l * l < range; l++) {
+                        int s2 = k * k * k + l * l * l;
+                        if (s1 == s2) {
+                            System.out.println(i + "^3 + " + j + "^3 == " + k + "^3 + " + l + "^3 = " + s1);
                         }
                     }
                 }
