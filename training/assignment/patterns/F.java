b8eb6148656b1745f6235664172a64f72ebe55f0
karthik1772 <karthikamma2004@gmail.com>
2025-02-24 10:20:32 +0530

    completed

---
+++ training/assignment/patterns/F.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class F {
+    static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i==0||j==0|i==n/2)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println(" ");
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        pattern(n);
+    }
+}
