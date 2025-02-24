b9ab934c70d8d72c82270e875b16ec4f2c3fa53a
karthik1772 <karthikamma2004@gmail.com>
2025-02-24 12:12:25 +0530

    completed

--- training/assignment/patterns/X.java
+++ training/assignment/patterns/X.java
@@ -1,12 +1,11 @@
+
 import java.util.Scanner;
 
-public class G {
+public class X {
     static void pattern(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == 0&&j<3*n/4 || j == 0 || i == n - 1 && j < 3 * n / 4 || j == 3 * n / 4 && i > n / 2
-                        || i == n / 2 && j > n / 2 && j < 3 * n / 4 || j == n / 2 && i > n / 2 && i < 3 * n / 4
-                        || i == 3 * n / 4 && j < n / 2 && j > n / 4||j==n/4&&i>n/4&&i<3*n/4||i==n/4&&j>n/4||j==n-1&&i>n/4)
+                if (i + j == n - 1 || i - j == 0)
                     System.out.print("*");
                 else
                     System.out.print(" ");
