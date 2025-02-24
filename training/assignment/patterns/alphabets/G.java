43f0c2f3b62da9aecbd3bf326e6e057c6851324d
karthik1772 <karthikamma2004@gmail.com>
2025-02-24 12:20:17 +0530

    completed

--- training/assignment/patterns/alphabets/G.java
+++ training/assignment/patterns/alphabets/G.java
@@ -1,11 +1,13 @@
-
 import java.util.Scanner;
 
-public class Z {
+public class G {
     static void pattern(int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i + j == n-1 ||i==0||i==n-1)
+                if (i == 0 && j < 3 * n / 4 || j == 0 || i == n - 1 && j < 3 * n / 4 || j == 3 * n / 4 && i > n / 2
+                        || i == n / 2 && j > n / 2 && j < 3 * n / 4 || j == n / 2 && i > n / 2 && i < 3 * n / 4
+                        || i == 3 * n / 4 && j < n / 2 && j > n / 4 || j == n / 4 && i > n / 4 && i < 3 * n / 4
+                        || i == n / 4 && j > n / 4 || j == n - 1 && i > n / 4)
                     System.out.print("*");
                 else
                     System.out.print(" ");
