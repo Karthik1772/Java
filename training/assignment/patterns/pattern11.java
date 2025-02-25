962f9021f15d05c009dc31b933ccd11e608e8ec3
karthik1772 <karthikamma2004@gmail.com>
2025-02-25 16:06:30 +0530

    completed

--- training/assignment/patterns/pattern11.java
+++ training/assignment/patterns/pattern11.java
@@ -5,7 +5,7 @@ public class pattern11 {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= n; j++) {
                 if ((i > 0 && j == n - i))
-                    System.out.print("*  ");
+                    System.out.print("* ");
                 else
                     System.out.print(" ");
             }
