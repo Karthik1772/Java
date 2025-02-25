d467b4c03151eb77f5c7cc92241ffc0b6e25fbf3
karthik1772 <karthikamma2004@gmail.com>
2025-02-25 12:11:07 +0530

    completed

---
+++ training/assignment/patterns/pattern5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class pattern5 {
+    static void pattern(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2 * n; j++) {
+                if (i + j == n - 1 || i - j == -n + 1 || i == n - 1 && j <= 2 * n - 2 ||
+                        i == n / 2 && j >= 3 * n / 4 && j <= (n - 1) + n / 4 ||
+                        i <= 3 * n / 4 && i >= n / 2 && (i - j == -n / 4 || i + j == (n - 1) + 3 * n / 4))
+                    System.out.print("* ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+        }
+        return;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        pattern(n);
+    }
+}
