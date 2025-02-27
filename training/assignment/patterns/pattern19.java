212f00b6149452c696a3b2f8289f77d76ba8584a
karthik1772 <karthikamma2004@gmail.com>
2025-02-27 09:21:35 +0530

    completed

---
+++ training/assignment/patterns/pattern19.java
@@ -0,0 +1,19 @@
+
+public class pattern19 {
+    public static void main(String[] args) {
+        int n=11; 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <n; j++) {
+               if(i-j>=0&&i+j<=n-1)
+               System.out.print(j+1);
+               else
+               System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+     
+    
+    }
+    
+    
+}
---
+++ training/assignment/patterns/pattern47.java
@@ -0,0 +1,22 @@
+class pattern47{
+        static void pattern(int n)
+        {
+            for(int i =0;i<2*n-1;i++)
+            {
+                for(int j=0;j<n;j++){
+                    if(i-j>0 && i+j<=2*n-1)
+                    if(i<n)
+                    System.out.print(n-1-i+j);
+                    else if(i>=n)
+                    System.out.print(i-(n-1)+j);  
+               }
+            System.out.println();
+            
+            }
+            
+        }
+        public static void main(String[] args) {
+            
+            pattern(5);
+        }
+    }
