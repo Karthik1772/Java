c1b1ca83d4c188c3dbf35c7a8344101c70674fa8
karthik1772 <karthikamma2004@gmail.com>
2025-03-30 12:27:03 +0530

    completed

--- kunal_kushwa/05-arrays/MaxSubarrayBruteForce.java
+++ kunal_kushwa/05-arrays/MaxSubarrayBruteForce.java
@@ -2,22 +2,25 @@ public class MaxSubarrayBruteForce {
     public static int maxSubarraySum(int[] arr) {
         int n = arr.length;
         int maxSum = Integer.MIN_VALUE;
-        
+
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
                 int currentSum = 0;
                 for (int k = i; k <= j; k++) {
-                    currentSum += arr[k];
+                    System.out.print(arr[k]);
+                    currentSum = currentSum + arr[k];
                 }
+                System.out.print(" ");
                 maxSum = Math.max(maxSum, currentSum);
             }
+            System.out.println();
         }
-        
+
         return maxSum;
     }
-    
+
     public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        int[] arr = { 1, 2, 3, 4, 5 };
         System.out.println("Maximum Subarray Sum: " + maxSubarraySum(arr));
     }
 }
--- kunal_kushwa/05-arrays/binary.java
+++ kunal_kushwa/05-arrays/binary.java
@@ -10,10 +10,10 @@ public class binary {
         int right=arr.length-1;
         while(left<=right){
             int mid=(left+right)/2;
-            if(arr[mid]==target){
+            if(arr[mid][]==target){
                 return mid;
             }
-            else if(arr[mid]<target){
+            else if(arr[mid][]<target){
                 left=mid+1;
             }
             else{
