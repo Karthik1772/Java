e5660edd93a555ac0f320582f4290522d7c83245
karthik1772 <karthikamma2004@gmail.com>
2026-08-04 12:16:05 +0530

    updated

--- basic training/day7/array14.java
+++ basic training/day7/array14.java
@@ -12,12 +12,28 @@ public class array14 {
         int a[] = { 1, 2, 3, 4, 5 };
         int n = a.length;
         int r = 2;
-        r = r % n;
-        rev(a, 0, r - 1);
-        rev(a, r, n - 1);
-        rev(a, 0, n - 1);
-        for (int i = 0; i < n; i++) {
-            System.out.print(a[i] + " ");
+        // r = r % n;
+        // rev(a, 0, r - 1);
+        // rev(a, r, n - 1);
+        // rev(a, 0, n - 1);
+        // for (int i = 0; i < n; i++) {
+        //     System.out.print(a[i] + " ");
+        // }
+        swap(a, n, r);
+    }
+    static void swap(int a[], int n, int r) {
+        int[] new1 = new int[n];
+        
+        for (int i = r, j = 0; i < n; i++, j++) {
+            new1[j] = a[i];
+        }
+
+        for (int i = n - r, j = 0; i < n; i++, j++) {
+            new1[i] = a[j];
+        }
+
+        for (int x : new1) {
+            System.out.print(x + " ");
         }
     }
 }
